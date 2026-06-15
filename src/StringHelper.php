eb15f383aea876e336d96505c5f168645c10c175
Filipe <filipefranciscof3@gmail.com>
2026-06-14 22:42:16 -0300

    update: 1 arquivo(s) modificado(s)

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -1,8 +1,17 @@
 <?php
 
+/**
+ * Classe responsável por manipulações de strings, como a geração de slugs e truncamento de texto.
+ */
 class StringHelper
 {
 
+    /**
+     * Converte uma string em um formato "slug", substituindo caracteres não alfanuméricos por hífens e convertendo tudo para minúsculas.
+     * 
+     * @param mixed $text A string a ser convertida em slug.
+     * @return mixed A string formatada como slug.
+     */
     public function slugify($text)
     {
         $text = strtolower($text);
@@ -10,6 +19,13 @@ class StringHelper
         return trim($text, '-');
     }
 
+    /**
+     * Trunca uma string para um limite especificado e adiciona reticências se a string for maior que o limite.
+     * 
+     * @param mixed $text A string a ser truncada.
+     * @param mixed $limit O limite de caracteres para a string truncada.
+     * @return mixed A string truncada ou a original se não exceder o limite.
+     */
     public function truncate($text, $limit)
     {
         if (strlen($text) <= $limit) {
